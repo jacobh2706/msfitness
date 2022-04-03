bbb0dd4168864b3fcdb02a09935b2cdefc7f7b89
Jacob <jacob2003@gmx.at>
2022-04-03 18:44:39 +0200

    Modal Content Padding

--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -85,7 +85,7 @@ const HomeContent = ({ isloaded, isBday }) => {
                             <IonTitle size='large'>Modal Content</IonTitle>
                         </IonToolbar>
                     </IonHeader>
-                    <IonContent fullscreen="true" scrollY="false">
+                    <IonContent fullscreen="true" scrollY="false" className='modal-content'>
                         <IonText>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                         </IonText>
@@ -101,7 +101,7 @@ const HomeContent = ({ isloaded, isBday }) => {
                             <IonTitle size='large'>Modal Content</IonTitle>
                         </IonToolbar>
                     </IonHeader>
-                    <IonContent fullscreen="true" scrollY="false">
+                    <IonContent fullscreen="true" scrollY="false" className='modal-content'>
                         <IonText>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                         </IonText>
