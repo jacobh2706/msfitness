2094d8673edce4319a6599cf3e5401f78e109e47
Jacob <jacob2003@gmx.at>
2022-04-04 11:17:59 +0200

    Edit Button

--- src/components/Detail_Header.js
+++ src/components/Detail_Header.js
@@ -1,9 +1,18 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonButtons, IonToolbar, IonBackButton, IonButton } from '@ionic/react';
 import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect, } from 'react';
 
 import './Detail_Header.css';
 
 const Detail_Header = ({ site_title }) => {
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem("user-role") == "ADMIN") {
+            setIsAdmin(true);
+        }
+    });
+
     return (
         <IonHeader>
             <IonToolbar>
@@ -11,9 +20,10 @@ const Detail_Header = ({ site_title }) => {
                     <IonBackButton defaultHref="../Equipment" />
                 </IonButtons>
                 <IonTitle>{site_title}</IonTitle>
+                {isAdmin && 
                 <IonButtons slot="primary">
                     <IonButton>Bearbeiten</IonButton>
-                </IonButtons>
+                </IonButtons>}
             </IonToolbar>
         </IonHeader>
     );
