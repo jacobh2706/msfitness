ab501c072ae6419b6b1dff04ddbc5cb6ade8f78c
Jacob <jacob2003@gmx.at>
2022-04-04 13:24:30 +0200

    Edit Function

--- src/components/Detail_Header.js
+++ src/components/Detail_Header.js
@@ -1,17 +1,31 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonButtons, IonToolbar, IonBackButton, IonButton } from '@ionic/react';
 import { NavLink } from 'react-router-dom';
 import React, { useState, useEffect, } from 'react';
+import ReactDOM from 'react-dom';
 
 import './Detail_Header.css';
 
 const Detail_Header = ({ site_title }) => {
     const [isAdmin, setIsAdmin] = useState(false);
+    
 
     useEffect(() => {
         if (localStorage.getItem("user-role") == "ADMIN") {
             setIsAdmin(true);
         }
     });
+
+    const activateEditMode = () => {
+        localStorage.setItem("EditMode", true);
+        console.log("Edit Mode aktiviert");
+        window.location.reload(true);
+    }
+
+    const endEditMode = () => {
+        localStorage.setItem("EditMode", false);
+        console.log("Edit Mode deaktiviert");
+        window.location.reload(true);
+    }
 
     return (
         <IonHeader>
@@ -20,9 +34,16 @@ const Detail_Header = ({ site_title }) => {
                     <IonBackButton defaultHref="../Equipment" />
                 </IonButtons>
                 <IonTitle>{site_title}</IonTitle>
+                
                 {isAdmin && 
+                 localStorage.getItem("EditMode") == "false" && 
                 <IonButtons slot="primary">
-                    <IonButton>Bearbeiten</IonButton>
+                    <IonButton onClick={activateEditMode}>Bearbeiten</IonButton>
+                </IonButtons>}
+                {isAdmin && 
+                 localStorage.getItem("EditMode") == "true" && 
+                <IonButtons slot="primary">
+                    <IonButton onClick={endEditMode}>Fertig</IonButton>
                 </IonButtons>}
             </IonToolbar>
         </IonHeader>
--- src/components/Equipment/DetailContent.js
+++ src/components/Equipment/DetailContent.js
@@ -1,11 +1,24 @@
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons } from '@ionic/react';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, } from 'react';
 import ReactPlayer from 'react-player'
 import video from '../../images/Rum_kreuzheben.mov';
+
 
 import './DetailContent.css';
 
 const DetailContent = ({ site_title,site_descr,video_link }) => {
+    const [editMode, setEditMode] = useState(false);
+    
+    useEffect(() => {
+        if (localStorage.getItem("EditMode") == "true") {
+            setEditMode(true);
+            console.log("editmode")
+        } else {
+            setEditMode(false);
+            console.log("editmode off")
+        }
+    });
+
     return (
         <IonContent fullscreen="true">
             <IonHeader collapse="condense">
@@ -27,6 +40,21 @@ const DetailContent = ({ site_title,site_descr,video_link }) => {
                     />
                 </div>
             </div>
+
+            {editMode && 
+            <div className='detail-content-edit'>
+                <textarea defaultValue={site_descr}></textarea>
+                <h1>Video</h1>
+                <div className='player-wrapper'>
+                    <ReactPlayer
+                        className='react-player'
+                        url={[video_link]}
+                        width='80%'
+                        height='100%'
+                        controls={true}
+                    />
+                </div>
+            </div>}
         </IonContent>
     );
 };
