e1e390543a4241feec6309ab059ff33319db5fde
Jacob <jacob2003@gmx.at>
2022-04-05 12:25:05 +0200

    User Liste

--- src/components/User/AdminSettings.js
+++ src/components/User/AdminSettings.js
@@ -1,14 +1,23 @@
-import { IonContent, IonHeader, IonList, IonItem, IonLabel } from '@ionic/react';
+import { IonContent, IonHeader, IonList, IonItem, IonLabel, IonSpinner } from '@ionic/react';
 import './UserContent.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import $ from 'jquery';
+import ReactDOM from 'react-dom';
 
 const Admin_Settings = () => {
     var Elems = [];
 
     useEffect(() => {
+        ReactDOM.render(
+            <IonSpinner />,
+            document.getElementById('admin-content')
+        );
         axios.get('https://msfitness-17584.nodechef.com/getalluserdata').then((result) => {
+            ReactDOM.render(
+                <IonList className='user-list' lines='full'></IonList>,
+                document.getElementById('admin-content')
+            );
             Elems.push(result.data);
             console.log(Elems);
             for (let i = 0; i < Elems[0].length; i++) {
@@ -20,13 +29,17 @@ const Admin_Settings = () => {
 
                 $('.user-label')[i].appendChild(document.createElement("h2")).appendChild(document.createTextNode(Elems[0][i].FIRSTNAME + " " + Elems[0][i].LASTNAME));
                 $('.user-label')[i].appendChild(document.createElement("p")).appendChild(document.createTextNode(Elems[0][i].EMAIL));
+
+                if (Elems[0][i].ROLE == "ADMIN") {
+                    var item = $('<p>' + Elems[0][i].ROLE + '</p>').addClass("user-role");
+                    $('.user-label')[i].append(item[0]);
+                }
             }
         });
     });
 
     return (
-        <IonContent className='admin-content'>
-            <IonList className='user-list' lines='full'></IonList>
+        <IonContent className='admin-content' id="admin-content">
         </IonContent>
     );
 };
